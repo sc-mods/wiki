4abf596a20e9845512b10410d5c0574924485349
Denis Ponomarev <star.mods.b@gmail.com>
2025-07-14 16:57:28 +0400

    Update sc-language.js

--- src/sc-language.js
+++ src/sc-language.js
@@ -9,15 +9,15 @@ class ScLanguage extends HTMLElement {
   connectedCallback() {
     const currentLang = getLang();
     this.innerHTML = `
-      <img id="lang-image" src="../assets/lang/${currentLang.substring(0,2).toLowerCase()}.svg" alt="Language" title="Language">
+      <img id="lang-image" src="https://star-assets.github.io/lang/${currentLang.substring(0,2).toLowerCase()}.svg" alt="Language" title="Language">
       <ul class="language-dropdown-menu" role="menu">
-        <li data-lang="pl"><img src="../assets/lang/pl.svg" alt="Polski" title="Polski"></li>
-        <li data-lang="fr"><img src="../assets/lang/fr.svg" alt="Français" title="Français"></li>
-        <li data-lang="es"><img src="../assets/lang/es.svg" alt="Español" title="Español"></li>
-        <li data-lang="en"><img src="../assets/lang/en.svg" alt="English" title="English"></li>
-        <li data-lang="ru"><img src="../assets/lang/ru.svg" alt="Русский" title="Русский"></li>
-        <li data-lang="kr"><img src="../assets/lang/kr.svg" alt="한국어" title="한국어"></li>
-        <li data-lang="cn"><img src="../assets/lang/cn.svg" alt="简体中文" title="简体中文"></li>
+        <li data-lang="pl"><img src="https://star-assets.github.io/lang/pl.svg" alt="Polski" title="Polski"></li>
+        <li data-lang="fr"><img src="https://star-assets.github.io/lang/fr.svg" alt="Français" title="Français"></li>
+        <li data-lang="es"><img src="https://star-assets.github.io/lang/es.svg" alt="Español" title="Español"></li>
+        <li data-lang="en"><img src="https://star-assets.github.io/lang/en.svg" alt="English" title="English"></li>
+        <li data-lang="ru"><img src="https://star-assets.github.io/lang/ru.svg" alt="Русский" title="Русский"></li>
+        <li data-lang="kr"><img src="https://star-assets.github.io/lang/kr.svg" alt="한국어" title="한국어"></li>
+        <li data-lang="cn"><img src="https://star-assets.github.io/lang/cn.svg" alt="简体中文" title="简体中文"></li>
       </ul>`
 
     // Attach event listeners to prevent default and change language
@@ -41,7 +41,7 @@ class ScLanguage extends HTMLElement {
   _updateUI() {
     let currentLang = getLang();
     if(currentLang == "enUS")currentLang = "en" //todo
-    this.querySelector("#lang-image").setAttribute("src",`../assets/lang/${currentLang.substring(2).toLowerCase()}.svg`)
+    this.querySelector("#lang-image").setAttribute("src",`https://star-assets.github.io/lang/${currentLang.substring(2).toLowerCase()}.svg`)
   }
 }
 
